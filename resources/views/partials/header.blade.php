362a9e25654f18ef519133463fb37a36d6d6e9f5
mattiavisona <mattiav2706@gmail.com>
2023-05-10 18:12:34 +0200

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header class="text-center">
+    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+
+    <ul>
+        <li>Link</li>
+        <li>Home</li>
+        <li>About us</li>
+        <li>Store</li>
+    </ul>
+</header>
